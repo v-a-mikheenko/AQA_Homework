d87b6df3a232036865da267cd0cc59aaaa2e7898
v-a-mikheenko <v.a.mikheenko@gmail.com>
2025-01-09 13:33:31 +0300

    Create Animal.java

---
+++ src/main/java/animals/Animal.java
@@ -0,0 +1,27 @@
+package animals;
+
+public abstract class Animal {
+    public String name;
+    private static int animalCount = 0;
+
+    public Animal(String name) {
+        this.name = name;
+        animalCount++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public abstract void run(int lenght);
+
+    public abstract void swim(int lenght);
+
+    public static void getAnimalCount() {
+        System.out.println("Количество животных: " + animalCount);
+    }
+}
