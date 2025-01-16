6be65a465d63717ba47b841aa69eb619e741786b
v-a-mikheenko <v.a.mikheenko@gmail.com>
2025-01-16 13:12:26 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,46 @@
-Class Main {
-	public static void main(String[] args){
-		System.out.println("main");
-	}
+public class Main {
+    public static void main(String[] args) {
+        String[][] array1 = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+        String[][] array2 = {
+                {"1", "2", "3"},
+                {"5", "6", "7"},
+                {"9", "10", "11"},
+                {"13", "14", "15"}
+        };
+
+        String[][] array3 = {
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8"},
+                {"9", "x", "11", "12"},
+                {"13", "14", "15", "16"}
+        };
+
+
+        try {
+            int result = ArraysCheck.arrayCheck(array1);
+            System.out.println("Сумма: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            int result = ArraysCheck.arrayCheck(array2);
+            System.out.println("Сумма: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+
+        try {
+            int result = ArraysCheck.arrayCheck(array3);
+            System.out.println("Сумма: " + result);
+        } catch (MyArraySizeException | MyArrayDataException e) {
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
 }
