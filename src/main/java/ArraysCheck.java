434db629fe12b738d1a04e7e3b76aa030fa399d0
v-a-mikheenko <v.a.mikheenko@gmail.com>
2025-01-16 13:12:32 +0300

    Create ArraysCheck.java

---
+++ src/main/java/ArraysCheck.java
@@ -0,0 +1,19 @@
+public class ArraysCheck {
+    public static int arrayCheck(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        if (array.length != 4 || array[0].length != 4) {
+            throw new MyArraySizeException("Неправильный размер массива");
+        }
+
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                try {
+                    sum += Integer.parseInt(array[i][j]);
+                } catch (NumberFormatException e) {
+                    throw new MyArrayDataException("Неверные данные в ячейке [" + i + "][" + j + "]");
+                }
+            }
+        }
+        return sum;
+    }
+}
