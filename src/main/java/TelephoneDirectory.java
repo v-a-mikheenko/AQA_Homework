1489bb6881d3f7eb383c2f91c8ca0f73a19b9cab
v-a-mikheenko <v.a.mikheenko@gmail.com>
2025-01-26 13:39:03 +0300

    Update TelephoneDirectory.java

--- src/main/java/TelephoneDirectory.java
+++ src/main/java/TelephoneDirectory.java
@@ -1,13 +1,19 @@
 import java.util.*;
 
 public class TelephoneDirectory {
-    private final Map<String, List<String>> telephoneDirectory = new HashMap<>();
+    private final Map<String, Set<String>> telephoneDirectory = new HashMap<>();
+    private final Set<String> set = new HashSet<>();
+
 
     public void add(String lastName, String telephoneNumber) {
-        telephoneDirectory.computeIfAbsent(lastName, k -> new ArrayList<>()).add(telephoneNumber);
+        if(!set.add(telephoneNumber)){
+            System.out.println(telephoneNumber + " - данный телефонный номер уже есть в справочнике");
+        } else {
+            telephoneDirectory.computeIfAbsent(lastName, p -> new HashSet<>()).add(telephoneNumber);
+        }
     }
 
-    public List<String> get(String lastName) {
-        return telephoneDirectory.getOrDefault(lastName, Collections.emptyList());
+    public Set<String> get(String lastName) {
+        return telephoneDirectory.getOrDefault(lastName, Collections.emptySet());
     }
 }
