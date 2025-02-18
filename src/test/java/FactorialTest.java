9f462ab7469ad7cf9a029159497ed161144be8a0
v-a-mikheenko <v.a.mikheenko@gmail.com>
2025-02-18 16:51:15 +0300

    Update FactorialTest.java

--- src/test/java/FactorialTest.java
+++ src/test/java/FactorialTest.java
@@ -1,62 +1,51 @@
-import org.junit.Test;
+
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import java.util.stream.Stream;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FactorialTest {
 
     @Test
     public void testZero() {
-        assertEquals(1, Main.getFactorial(0));
+        assertEquals(1, FactorialCalculator.getFactorial(0));
     }
 
     @ParameterizedTest
-    @CsvSource({"1, 1", "2, 2", "3, 6", "4, 24", "5, 120", "11, 39916800", "12, 479001600"})
-    public void testPositiveValues(int number, int result) {
-        assertEquals(result, Main.getFactorial(number));
+    @CsvSource({"1, 1", "2, 2", "5, 120", "12, 479001600", "19, 121645100408832000", "20, 2432902008176640000"})
+    public void testPositiveValues(int number, long result) {
+        assertEquals(result, FactorialCalculator.getFactorial(number));
     }
 
     @Test
     public void testNegativeNumber() {
-        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> Main.getFactorial(-1));
+        Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.getFactorial(-1));
 
         String expectedMessage = "Число должно быть неотрицательным";
         String actualMessage = exception.getMessage();
 
-        Assertions.assertEquals(expectedMessage, actualMessage);
+        assertEquals(expectedMessage, actualMessage);
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {13, 14, 15, 50})
+    @ValueSource(ints = {21, 22, 23, 50})
     public void testGrateInt(int number) {
-        Exception exception = Assertions.assertThrows(ArithmeticException.class, () -> Main.getFactorial(number));
+        Exception exception = Assertions.assertThrows(ArithmeticException.class, () -> FactorialCalculator.getFactorial(number));
 
-        String expectedMessage = "Слишом большой результат расчета для типа данных int";
+        String expectedMessage = "Слишком большой результат расчета для типа данных long";
         String actualMessage = exception.getMessage();
 
-        Assertions.assertEquals(expectedMessage, actualMessage);
+        assertEquals(expectedMessage, actualMessage);
     }
 
-    private static Stream<Object> wrongType() {
-        return Stream.of("five", true, 5.5);
-    }
-
-    @ParameterizedTest
-    @MethodSource("wrongType")
-    void testWrongType(Object input) {
-        Assertions.assertThrows(ClassCastException.class, () -> Main.getFactorial((int) input));
-    }
 
     @ParameterizedTest
     @NullSource
     void testNullInput(Integer input) {
-        Assertions.assertThrows(NullPointerException.class, () -> Main.getFactorial(input));
+        Assertions.assertThrows(NullPointerException.class, () -> FactorialCalculator.getFactorial(input));
     }
 }
